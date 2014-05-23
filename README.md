7cddeeec244c9763b2ffe8cfec1a8b2b57d83169
Sebastian Clausen <s.clausen@greylogix.de>
2014-05-23 12:03:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+meteor-bootstrap-colorpicker-2
+============================
+
+bootstrap colorpicker 2 packages for meteor
---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "Bootstrap Colorpicker 2 packaged for meteor"
+});
+
+Package.on_use(function(api) {
+  api.add_files([
+    "lib/bootstrap-colorpicker/js/bootstrap-colorpicker.js",
+    "lib/bootstrap-colorpicker/css/bootstrap-colorpicker.css",
+    "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/alpha.png",
+    "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/hue.png",
+    "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/saturation.png"
+  ], "client");
+
+  api.add_files('path-override.css', 'client');
+});
